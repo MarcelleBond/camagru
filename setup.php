335818cf86415fad199edefa11cc7a8aa59e2768
Marcelle BOND <mbond@e4r10p3.wethinkcode.co.za>
2018-10-15 17:04:44 +0200

    goood start

--- setup.php
+++ setup.php
@@ -3,10 +3,9 @@
 
     try {
         $dbh = new PDO("mysql:host=$DB_DNS", $DB_USER, $DB_PASSWORD);
-
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $dbh->exec("CREATE DATABASE IF NOT EXISTS $DB_NAME;")
         or die(print_r($dbh->errorInfo(), true));
-        // $dbh->use($DB_NAME) or die(print_r($dbh->errorInfo(), true));
         $dbh->exec("CREATE TABLE `camagru`.`users`(
             `id` INT NOT NULL AUTO_INCREMENT,
             `username` VARCHAR(20) NOT NULL,
@@ -36,6 +35,24 @@
             NULL,
             'Administrator',
             '{\"admin\": 1}'
+        )");
+        $dbh->exec("CREATE TABLE `camagru`.`gallery`(
+            `img_id` INT NOT NULL AUTO_INCREMENT,
+            `img_name` VARCHAR(255) NOT NULL,
+            `img_blob` LONGBLOB NOT NULL,
+            `user_id` INT NOT NULL,
+            `comments_id` INT NOT NULL,
+            `time_stamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            PRIMARY KEY(`img_id`)
+        )");
+        $dbh->exec("CREATE TABLE `camagru`.`comments`(
+            `comment_id` INT NOT NULL AUTO_INCREMENT,
+            `user_img_id` INT NOT NULL,
+            `friend_id` INT NOT NULL,
+            `comment` VARCHAR(255) NOT NULL,
+            `time_stamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            `img_id` INT NOT NULL,
+            PRIMARY KEY(`comment_id`)
         )");
     } catch (PDOException $e) {
         die("DB ERROR: ". $e->getMessage());
