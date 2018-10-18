c6d659bf99daed01825f5235e2d84e6dc740eaa5
MarcelleBond <mbond@student.wethinkcode.co.za>
2018-10-18 21:40:59 +0200

    good work

--- core/init.php
+++ core/init.php
@@ -20,7 +20,7 @@
 	);
 
 	spl_autoload_register(function($class){
-		require_once 'classes/' . $class . '.php';
+		require_once 'classes/' . $class .'.class'. '.php';
 	});
 
 	require_once 'functions/sanitize.php';
