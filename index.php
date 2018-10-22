45c1749d838fdb3eb6164b6d6f84423a48904134
MarcelleBond <mbond@student.wethinkcode.co.za>
2018-10-22 20:43:26 +0200

    good work

--- index.php
+++ index.php
@@ -1,16 +1,6 @@
 <?php
  	require_once 'core/init.php';
 
-/*	$user = DB::getInstance()->insert('users',array(
-		'username' => 'Dale',
-		'password' => '123456',
-		'salt' => 'salt'));
-
-	if ($user) {
-		echo "yay";
-	} else {
-		echo "i fucked up";
-	} */
 
 	if(session::exists("home"))
 	{
