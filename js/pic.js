85055a7a78f551883cd8e5b6df7f3422fa4894cf
MarcelleBond <mbond@student.wethinkcode.co.za>
2018-10-23 22:57:27 +0200

    good work

--- js/pic.js
+++ js/pic.js
@@ -1,29 +1,77 @@
-// Global vars to user thro out script...
+window.onload = function()
+{
 
-let width = 500,
-	hight = 0,
-	filter = 'none',
-	streaming = false;
+	// Global vars to user thro out script...
 
-// DOM Elements that are required to take pictures
+	let width = 500,
+		height = 0,
+		filter = 'none',
+		streaming = false;
 
-const video = document.getElementById('video');
-const canvas = document.getElementById('canvas');
-const photos = document.getElementById('photos');
-const photo_button = document.getElementById('photo_button');
-const clear_button = document.getElementById('clear_button');
-const photo_filter = document.getElementById('photo_filter');
+	// DOM Elements that are required to take pictures
 
-// get the media stream to use the webcam
+	const video = document.getElementById('video');
+	const canvas = document.getElementById('canvas');
+	const photos = document.getElementById('photos');
+	const photo_button = document.getElementById('photo_button');
+	const clear_button = document.getElementById('clear_button');
+	const photo_filter = document.getElementById('photo_filter');
 
-navigator.mediaDevices.getUserMedia({video: true, audio: false})
+	// get the media stream to use the webcam
 
-if (navigator.mediaDevices.getUserMedia) {       
-    navigator.mediaDevices.getUserMedia({video: true})
-  .then(function(stream) {
-    video.srcObject = stream;
-  })
-  .catch(function(error) {
-    console.log("Something went wrong!");
-  });
+	navigator.mediaDevices.getUserMedia({video: true, audio: false})
+
+	.then(function(stream){
+		// conects you to the webcam
+
+		video.srcObject = stream;
+
+		video.play();
+	})
+
+	.catch(function(err){
+		console.log(`Error: ${err}`);
+	});
+
+	video.addEventListener('canplay', function(e){
+		if (!streaming) {
+
+			height = video.videoHeight / (video.videoWidth / width);
+			video.setAttribute('width',width);
+			video.setAttribute('height',height);
+			canvas.setAttribute('width',width);
+			canvas.setAttribute('height',height);
+
+			streaming = true;
+		}
+	}, false);
+
+	photo_button.addEventListener('click',function(e){
+		
+		takepicture();
+
+		e.preventDefault()
+	}, false);
+
+	function takepicture()
+	{
+		//creating the canvas
+		const context = canvas.getContext('2d');
+
+		if (width && height) {
+			// setting the canvas width and height
+			canvas.width = width;
+			canvas.height = height;
+			// placeing an image from the video to the canvas
+			context.drawImage(video, 0, 0, width, height);
+			// creating an image from the canvas 
+			const  imgUrl = canvas.toDataURL('image/png');
+			// creating and img element to display one the page..
+			const img = document.createElement('img');
+			// set the image source 
+			img.setAttribute('src', imgUrl);
+			//
+		}
+	}
+
 }
