7c68dbe494fdaeb19f037e6133beef494e67fd0b
Marcelle BOND <mbond@e4r10p3.wethinkcode.co.za>
2018-10-29 15:22:06 +0200

    good

--- js/pic.js
+++ js/pic.js
@@ -1,31 +1,29 @@
 window.onload = function()
 {
 
-	// Global vars to user thro out script...
+
+		var emo2 = document.getElementById('emoji1').addEventListener("click", ols);
+	
+	
+
+
 
 	let width = 500,
 		height = 0,
 		filter = 'none',
 		streaming = false;
 
-	// DOM Elements that are required to take pictures
-
 	const video = document.getElementById('video');
 	const canvas = document.getElementById('canvas');
 	const photos = document.getElementById('photos');
 	const photo_button = document.getElementById('photo_button');
-	const clear_button = document.getElementById('clear_button');
 	const photo_filter = document.getElementById('photo_filter');
-
-	// get the media stream to use the webcam
+	const save_photo = document.getElementById('save_photo');
 
 	navigator.mediaDevices.getUserMedia({video: true, audio: false})
 
 	.then(function(stream){
-		// conects you to the webcam
-
 		video.srcObject = stream;
-
 		video.play();
 	})
 
@@ -46,43 +44,70 @@ window.onload = function()
 		}
 	}, false);
 
-	// photo button event
-	photo_button.addEventListener('click',function(e){
-		photos.innerHTML = "";		
+	photo_button.addEventListener('click',function(e)
+	{
 		takepicture();
-
 		e.preventDefault()
 	}, false);
 
-	photo_filter.addEventListener('change', function(e){
-		//set filter to users choice;
-		
+	save_photo.addEventListener('click',function(e)
+	{
+		savepic()
+		e.preventDefault();
+	}, false);
+
+	photo_filter.addEventListener('change', function(e)
+	{
 		filter = e.target.value;
 		video.style.filter = filter;
 		e.preventDefault()
 	});
-	//  take pic from canvas
+
 	function takepicture()
 	{
-		//creating the canvas
 		const context = canvas.getContext('2d');
 
 		if (width && height) {
-			// setting the canvas width and height
 			canvas.width = width;
 			canvas.height = height;
-			// placeing an image from the video to the canvas
+
 			context.drawImage(video, 0, 0, width, height);
-			// creating an image from the canvas 
-			const  imgUrl = canvas.toDataURL('image/png');
-			// creating and img element to display one the page..
+			
+			// var emoji = new Image();
+			// emoji.src = filter;
+			// context.drawImage(emoji, 0, 0, width, height);
+			
 			const img = document.createElement('img');
-			// set the image source 
-			img.setAttribute('src', imgUrl);
-			img.style.filter = filter;
-			// ading images to the photo div
+			img.setAttribute('src', filter);
 			photos.appendChild (img);
 		}
 	}
 
+	function savepic(){
+		var dataURL = canvas.toDataURL();
+		const form = document.createElement('form');
+		form.action = 'webcamupload.php';
+		form.method = 'post';
+		const homeInput = document.createElement('input');
+		homeInput.type = 'hidden';
+		homeInput.name = 'img64';
+		homeInput.value = dataURL;
+		form.appendChild(homeInput);
+		document.body.appendChild(form);
+		form.submit();
+		}
+
+
+		function ols()
+		{
+			//canvas,video
+			alert('finally');
+		}
+
+
+		///////////////////////////
+
+		
 }
+
+
---
+++ newpic.php
@@ -0,0 +1,60 @@
+<!-- <?php
+	require_once 'core/init.php';
+?> -->
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Camagru</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" media="screen" href="css/pic.css"/>
+	<script src="js/pic.js"></script>
+	<script src="js/test.js"></script>
+</head>
+<body>
+	<div class="navbar">
+		<h1>Camagru</h1>
+	</div>
+
+	<div class="top_container">
+
+	<!--  -->
+
+		<video id='video'>Stream not available...</video>
+
+	<!--  -->
+		<button id="photo_button" class="btn btn_darkk">
+			Take Photo
+		</button>
+		<button id="save_photo" class="btn btn_darkk">
+			save
+		</button>
+		
+			<img height='100px' width='100px' id="emoji1" name="emoji1" src="images/emojis/emoji_1.jpg">
+		
+		<select id="photo_filter">
+			<option value="none">Normal</option>
+			<option value="images/emojis/emoji_1.jpg">Grayscale</option>
+			<option value="images/emojis/emoji_2.jpg">Sepia</option>
+			<option value="images/emojis/emoji_3.jpg">Invert</option>
+			<option value="images/emojis/emoji_4.jpg">Hue</option>
+			<option value="images/emojis/emoji_5.jpg">Blur</option>
+			<option value="images/emojis/emoji_6.jpg">Contrast</option>
+			<option value="images/emojis/emoji_7.jpg">Contrast</option>
+			<option value="images/emojis/emoji_8.jpg">Contrast</option>
+			<option value="images/emojis/emoji_9.jpg">Contrast</option>
+			<option value="images/emojis/emoji_10.jpg">Contrast</option>
+		</select>
+		<canvas id="canvas"></canvas>
+	</div>
+
+	<div class="bottom_container">
+		<div id="photos"></div>
+	</div>
+
+</body>
+
+
+</html>
