3e438cf74e8cbe544ed934188cf5a76abe478c67
Marcelle BOND <mbond@e4r10p3.wethinkcode.co.za>
2018-10-31 16:43:57 +0200

    good work

--- webupload.php
+++ webupload.php
@@ -5,22 +5,21 @@
      $user = new user();
     $imgnum = $db->get("gallery",array('user_id', '=', $user->data()->user_id));
     $data = explode( ',', $_POST["img64"] );
-    $emo = $_POST['emoji64'];
+    $emo = $_POST["emoji64"]; //explode( '/', $_POST["emoji64"] );
     $test = base64_decode($data[1]);
 //try imgcreate
     $user_id = $user->data()->user_id;
-    // $img_id = $imgnum->first()->img_id;
     file_put_contents("images/gallary/".$user_id."1.png", $test);
+
     $dest= imagecreatefrompng("images/gallary/".$user_id."1.png");
 
     $src = imagecreatefrompng($emo);
-    imagecopymerge($dest, $src, 10, 10, 0, 0, 100, 47, 75);
-//    echo "<img src=".$emo."><br>";
-    // header('Content-Type: image/png');
-    // imagepng($dest);
-    imagejpeg($dest, 'images/gallary/'.$user_id.'2.png');
+    $width = ImageSx($src);
 
-    //file_put_contents("images/gallary/".$user_id."2.png", imagecopymerge($src, $dest, 10, 10, 0, 0, 100, 47, 75));
-   // echo imagepng($dest);
+    $height = ImageSy($src);
+    $x = $width/4; $y = $height/4;
+    ImageCopyResampled($dest,$src,0,0,0,0,$x,$y,$width,$height);
+    imagepng($dest, 'images/gallary/'.$user_id.'2.png');
+
 
 ?>
